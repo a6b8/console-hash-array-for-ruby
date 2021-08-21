2297c46999441828eb330e39b21e7fd0a82fc86b
a6b8 <andreasbanholzer@me.com>
2021-08-21 16:42:13 +0200

    initial commit

--- test/test.rb
+++ test/test.rb
@@ -1,11 +1,37 @@
 require '../lib/console_hash_array'
 
 
-groups = ConsoleHashArray.example()
-groups.keys.each.with_index do | key, rindex |
-    p = ConsoleHashArray.console( nil, groups, rindex, nil, key, :left, { test:'' } )
-    groups[ key ].each.with_index do | item, cindex |
-        p = ConsoleHashArray.console( p, groups, rindex, cindex, key, :right )
-    end
+tests = [
+  { test:'' },
+  { 
+    right__boxes_total: 20,
+    style__steps: '#',
+    print__left: "{{rindex}}. {{key}} {{left__spaces}}   "
+  },
+  { 
+    right__boxes_total: 10,
+    style__spaces: '.',
+    style__steps: '#',
+    print__left: "{{rindex}}.{{key}}{{left__spaces}}.."
+  },
+  {
+    er: 'deded',
+    ddde: 'dedede',
+    right__boxes_total: 50,
+  }
+]
+
+tests.each do | test |
+  groups = ConsoleHashArray.example()
+
+  groups.keys.each.with_index do | key, rindex |
+      ConsoleHashArray.console( groups, rindex, nil, key, :left, test )
+      groups[ key ].each.with_index do | item, cindex |
+          ConsoleHashArray.console( groups, rindex, cindex, key, :right )
+      end
+  end
+
+  puts
   puts
 end
+
